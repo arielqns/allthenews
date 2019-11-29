1241292648a4ac503e0996a8f19db3c5a5ddc61b
arielqns <arielqns@gmail.com>
2019-11-29 14:08:16 -0500

    added notes.js

---
+++ models/notes.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// noteSchema 
+var noteSchema = new Schema({
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+  date: String,
+  noteText: String
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Note = mongoose.model("Note", noteSchema);
+
+// Export the Note model
+module.exports = Note;
